664646c9f3550085785cb5019a5ad146a448b5d8
Sahra Vadrot <apexp@hotmail.fr>
2018-11-08 12:53:44 +0100

    add animation key

--- assets/js/start-the-game.js
+++ assets/js/start-the-game.js
@@ -18,12 +18,26 @@ AFRAME.registerComponent('start-the-game', {
             var manta_song = document.getElementById('manta_song');
             manta_song.setAttribute("src", "assets/song/manta.wav");
             manta_song.setAttribute("volume", 350);
-            
             let manta_audio = manta_song.components.sound;
+
+            var animation = document.createElement("a-animation");
+            var animationData = {
+                attribute: 'position',
+                dur: 3000,
+                from: '-39 1.4 -15',
+                to: '-39 1.4 -14'
+            };
+            Object.keys(animationData).forEach(function (attr) {
+                animation.setAttribute(attr, animationData[attr]);
+            });
+
+            el.appendChild(animation);
+
+            setTimeout(function(){ el.parentNode.removeChild(el); }, 3000)
 
             intro_audio.stopSound();
             xana_audio.playSound();
-            setTimeout(function(){ manta_audio.playSound(); }, 10000);
+            setTimeout(function () { manta_audio.playSound(); }, 10000);
         });
     }
 });

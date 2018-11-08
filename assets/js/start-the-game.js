d7f0b80bd8d03afc1d18971a247e670f97feb206
Sahra Vadrot <apexp@hotmail.fr>
2018-11-08 12:33:02 +0100

    complete start the game

--- assets/js/start-the-game.js
+++ assets/js/start-the-game.js
@@ -10,8 +10,20 @@ AFRAME.registerComponent('start-the-game', {
         });
         el.addEventListener('die', () => {
             var intro_song = document.getElementById("intro_song");
+            let intro_audio = intro_song.components.sound;
+
             var xana_song = document.getElementById("xana_song");
-            console.log(intro_song, xana_song);
+            let xana_audio = xana_song.components.sound;
+
+            var manta_song = document.getElementById('manta_song');
+            manta_song.setAttribute("src", "assets/song/manta.wav");
+            manta_song.setAttribute("volume", 350);
+            
+            let manta_audio = manta_song.components.sound;
+
+            intro_audio.stopSound();
+            xana_audio.playSound();
+            setTimeout(function(){ manta_audio.playSound(); }, 10000);
         });
     }
 });
